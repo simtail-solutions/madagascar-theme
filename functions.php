b9c7f550d13bb9323dbbf97d281e68e938b681eb
Adam Trickett <me@ariom.id.au>
2023-03-15 12:21:23 +0800

    Update functions.php
    fixed functions.php typo: filectime (not filetime)

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 function simtail_theme_styles_scripts() {
-    wp_register_style('theme_style', get_stylesheet_uri(), array(), filetime(get_stylesheet_directory().'/styles.css'));
-	wp_register_script('theme_script', get_stylesheet_directory_uri() . '/simtail.js', array('jquery'), filetime(get_stylesheet_directory().'/simtail.js'));
+    wp_register_style('theme_style', get_stylesheet_uri(), array(), filectime(get_stylesheet_directory().'/styles.css'));
+	wp_register_script('theme_script', get_stylesheet_directory_uri() . '/simtail.js', array('jquery'), filectime(get_stylesheet_directory().'/simtail.js'));
 	wp_enqueue_style('theme_style');
 	wp_enqueue_script(array('jquery'));
 	wp_enqueue_script('theme_script');
@@ -10,8 +10,8 @@ function simtail_theme_styles_scripts() {
 add_action('wp_enqueue_scripts', 'simtail_theme_styles_scripts');
 
 function simtail_admin_styles_scripts() {
-    wp_register_style('admin_style',  get_stylesheet_directory_uri() . '/simtail-admin.css', array(), filetime(get_stylesheet_directory().'/simtail-admin.css'));
-    wp_register_style('admin_script',  get_stylesheet_directory_uri() . '/simtail-admin.js', array('jquery'), filetime(get_stylesheet_directory().'/simtail-admin.js'));
+    wp_register_style('admin_style',  get_stylesheet_directory_uri() . '/simtail-admin.css', array(), filectime(get_stylesheet_directory().'/simtail-admin.css'));
+    wp_register_style('admin_script',  get_stylesheet_directory_uri() . '/simtail-admin.js', array('jquery'), filectime(get_stylesheet_directory().'/simtail-admin.js'));
     wp_enqueue_style('admin_style');
     wp_enqueue_style('admin_style');
 }
