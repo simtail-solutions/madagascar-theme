c3d0d925b27289a50e636824096e35363ca74c30
Adam Trickett <me@ariom.id.au>
2023-03-15 11:49:55 +0800

    Gruntfile created and configured for basic web dev tasks. Uses Sass and BrowserSync. No linting or minifying plugins yet.

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+let sass = require('sass')
+let package = require('./package.json')
+
+module.exports = function (grunt) {
+	let contributors = package.contributors.map(a => a.name)
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		authors: contributors.join(', '),
+
+		themeHeader: '/* \n' +
+			'   Theme Name: <%= pkg.nicename %>\n' +
+			'   Theme URI: <%= pkg.homepage %>\n' +
+			'   Author: <%= authors %>\n' +
+			'   Author URI: https://simtail.com\n' +
+			'   Description: <%= pkg.description %>\n' +
+			'   Version: <%= pkg.version %>\n' +
+			'*/\n\n',
+
+		'dart-sass': {
+			theme: {
+				options: {
+					sourceMap: true
+				},
+				files: {
+					'./style.css': './src/style.scss',
+				}
+			}
+		},
+
+		usebanner: {
+			theme: {
+				options: {
+					position: 'top',
+					banner: '<%= themeHeader %>',
+					linebreak: true,
+				},
+				files: {
+					src: ['./style.css']
+				}
+			}
+		},
+
+		watch: {
+			theme: {
+				files: ['./src/*'],
+				tasks: ['dart-sass:theme', 'usebanner'],
+				options: { spawn: true }
+			},
+		},
+
+		browserSync: {
+			dev: {
+				bsFiles: {
+					src: './style.css'
+				},
+				options: {
+					watchTask: true,
+					proxy: "local.dev"
+				}
+			}
+		}
+	})
+
+	grunt.loadNpmTasks('grunt-banner')
+	grunt.loadNpmTasks('grunt-contrib-watch')
+	grunt.loadNpmTasks('grunt-dart-sass')
+	grunt.loadNpmTasks('grunt-browser-sync')
+
+	grunt.registerTask('default', ['browserSync', 'watch'])
+}
